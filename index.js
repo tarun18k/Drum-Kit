076ba1b449e48cd889d64000263d3ca1573f54ba
Tarun <45634649+tarun18k@users.noreply.github.com>
2020-04-15 16:22:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,42 +3,42 @@ for (var i = 0; i < document.querySelectorAll('button').length; i++) {
         var expression = this.innerHTML;
         switch (expression) {
             case "w":
-                var audi = new Audio("sounds/tom-1.mp3");
+                var audi = new Audio("tom-1.mp3");
                 audi.play();
                 animation(expression);
                 break;
             case "a":
-                var audi = new Audio("sounds/tom-2.mp3");
+                var audi = new Audio("tom-2.mp3");
                 audi.play();
                 animation(expression);
                 break;
 
             case "s":
-                var audi = new Audio("sounds/tom-3.mp3");
+                var audi = new Audio("tom-3.mp3");
                 audi.play();
                 animation(expression);
                 break;
 
             case "d":
-                var audi = new Audio("sounds/tom-4.mp3");
+                var audi = new Audio("tom-4.mp3");
                 audi.play();
                 animation(expression);
                 break;
 
 
             case "j":
-                var audi = new Audio("sounds/crash.mp3");
+                var audi = new Audio("crash.mp3");
                 audi.play();
                 animation(expression);
                 break;
 
             case "k":
-                var audi = new Audio("sounds/kick-bass.mp3");
+                var audi = new Audio("kick-bass.mp3");
                 audi.play();
                 animation(expression);
                 break;
             case "l":
-                var audi = new Audio("sounds/snare.mp3");
+                var audi = new Audio("snare.mp3");
                 audi.play();
                 animation(expression);
                 break;
@@ -49,53 +49,48 @@ document.addEventListener("keydown", function (e) {
         var expression = e.key;
         switch (expression) {
             case "w":
-                var audi = new Audio("sounds/tom-1.mp3");
+                var audi = new Audio("tom-1.mp3");
                 audi.play();
                 animation(expression);
                 break;
             case "a":
-                var audi = new Audio("sounds/tom-2.mp3");
+                var audi = new Audio("tom-2.mp3");
                 audi.play();
                 animation(expression);
                 break;
 
             case "s":
-                var audi = new Audio("sounds/tom-3.mp3");
+                var audi = new Audio("tom-3.mp3");
                 audi.play();
                 animation(expression);
                 break;
 
             case "d":
-                var audi = new Audio("sounds/tom-4.mp3");
+                var audi = new Audio("tom-4.mp3");
                 audi.play();
                 animation(expression);
                 break;
 
 
             case "j":
-                var audi = new Audio("sounds/crash.mp3");
+                var audi = new Audio("crash.mp3");
                 audi.play();
                 animation(expression);
                 break;
 
             case "k":
-                var audi = new Audio("sounds/kick-bass.mp3");
+                var audi = new Audio("kick-bass.mp3");
                 audi.play();
                 animation(expression);
                 break;
             case "l":
-                var audi = new Audio("sounds/snare.mp3");
+                var audi = new Audio("snare.mp3");
                 audi.play();
                 animation(expression);
                 break;
 
         }
 
-
-
-
-        // var audi=new Audio("sounds/tom-2.mp3");
-        //         audi.play();
     });
 function animation(e)
 {
